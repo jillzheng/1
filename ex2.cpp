26898f3845d7228f4b8a1d6150b70dc126158429
jillzheng <100131768+jillzheng@users.noreply.github.com>
2022-02-22 19:42:06 +0800

    Create ex2.cpp

---
+++ ex2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myAge = 14;
+  cout << "My name is Jill I am  " << myAge << " years old. my highit and wight is 160cm and 55kg I have one young sister and one young brother .";
+  return 0;
+}
