f1f95d4f6b5a77acd896774dea498229db76774a
jillzheng <100131768+jillzheng@users.noreply.github.com>
2022-02-22 20:00:34 +0800

    Create ex3.cpp

---
+++ ex3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myAge = 14;
+  int heighit =160 ;
+  int weight =55 ;
+  int sister=1 ;
+  int brother=1 ;
+  cout << "My name is Jill  " << myAge << " years old.";
+  cout << "my highit and wight is" <<heighit << " cm 55kg";
+  cout << "I have" <<sister << " sister and brother";
+}
